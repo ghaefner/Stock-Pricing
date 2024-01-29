380f7763d68ceac6282db4732c6566b3a3365a20
ghaefner <guillaume-haefner@gmx.de>
2024-01-29 19:47:05 +0100

    Add Base Url to Conf

--- conf.py
+++ conf.py
@@ -3,9 +3,13 @@ import os
 # Get API KEY
 AV_API_KEY = os.getenv('ALPHAVANTAGE_API_KEY').strip()
 
+# Base url of alpha vantage
+BASE_URL = 'https://www.alphavantage.co/query?'
+
 # Dictionary with Stocks to track
 DICT_STOCK_LABELS = {
     "AAPL": "Apple Inc.",
     "MSFT": "Microsoft Corporation",
     "GOOGL": "Alphabet Inc."
 }
+
