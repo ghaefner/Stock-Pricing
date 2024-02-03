8aa2401f06af1cf45179c167f5c5af1ae41863bb
ghaefner <guillaume-haefner@gmx.de>
2024-02-03 16:41:42 +0100

    Added time selection to app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st 
-from av_api import get_timeseries, calc_timeseries_new_currency
+from av_api import get_timeseries, calc_timeseries_new_currency, filter_data_by_cutoff
 from conf import DICT_STOCK_LABELS, STOCK
 from plot import plot_timeseries
 from datetime import datetime
@@ -11,7 +11,7 @@ def main():
     st.sidebar.title("Options")
 
     # Dropdown menu for selecting stock
-    selected_stock = st.sidebar.selectbox("Select a stock", list(DICT_STOCK_LABELS.keys()), index=0)
+    selected_stock = st.sidebar.selectbox("Select a stock", list(DICT_STOCK_LABELS.values()), index=0)
 
     # Dropdown menu for selecting data type
     selected_data = st.sidebar.selectbox("Select data type", [STOCK.OPEN, STOCK.HIGH, STOCK.CLOSE, STOCK.LOW], index=2)
@@ -22,16 +22,27 @@ def main():
     # Dropdown menu for selecting currency
     selected_currency = st.sidebar.selectbox("Select currency", ["USD", "EUR", "GBP", "CAD", "JPY", "CNY"], index=0)
 
-    # Get timeseries data
-    data = get_timeseries(selected_stock, selected_periodicity)
+  
+    # Slider for selecting time range
+    if ( selected_periodicity == "daily"):
+        selected_time_range = st.sidebar.slider("Select time range (days)", 1, 365, 30)
+    elif (selected_periodicity == "weekly"):
+        selected_time_range = st.sidebar.slider("Select time range (weeks)", 1, 156, 10)
+    elif (selected_periodicity == "monthly"):
+        selected_time_range = st.sidebar.slider("Select time range (months)", 1, 120, 12)
 
+    # Get timeseries data according to perdiodicity and time range
+    if (selected_periodicity == "daily" and selected_time_range > 100):
+        data = get_timeseries(selected_stock, selected_periodicity, outputsize="full")
+    else:
+        data = get_timeseries(selected_stock, selected_periodicity)
+
+    data = filter_data_by_cutoff(data, n_cutoff=selected_time_range)
+    
     # Calculate new currency if it is selected
     if selected_currency != "USD":
         data = calc_timeseries_new_currency(stock_data=data, to_currency=selected_currency)
-    
 
-     # Slider for selecting time range
-    selected_time_range = st.sidebar.slider("Select time range (months)", 1, 36, 12)
     
 
     # Plot selected data against timestamp
